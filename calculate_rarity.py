6c1f62be4e48f86c8b4bcc3cfadf6c5859d10e25
lza_menace <lza_menace@protonmail.com>
2022-05-17 13:11:19 -0700

    update rarity lib and create initial script

---
+++ calculate_rarity.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import requests, json
+from os.path import exists
+from os import stat
+from time import sleep
+
+from gallery.collections import all_collections
+from gallery.collections import Collection as LocalCollection
+from gallery.library.rarity import Item, Category, Collection
+from gallery import config
+
+
+for collection in [LocalCollection(k) for k in all_collections]:
+    rarity_collection = Collection()
+    if collection.url_slug != 'non-fungible-soup':
+        continue
+    print(f'[+] Reading metadata for {collection.title} - {collection.contract_address}')
+    _p = f'{config.DATA_PATH}/{collection.contract_address}'
+    for token_id in range(collection.token_start, collection.token_end + 1):
+        data = None
+        p = f'{_p}/{str(token_id)}.json'
+
+        if not exists(p) or stat(p).st_size == 0:
+            print(f'{p} does not exist or is empty. Rarity rankings will be incomplete. Skipping collection.')
+            sleep(3)
+            continue
+
+        with open(p, 'r') as f:
+            try:
+                data = json.loads(f.read())
+            except:
+                print(f'Unable to load JSON data from {p}. Rarity rankings will be incomplete. Skipping collection.')
+                sleep(3)
+                continue
+
+        # Loop over all token metadata and add items to collection and count them
+        new_item = Item(token_id, data)
+        rarity_collection.items.append(new_item)
+
+        # Loop over items in collection to get list of all category/trait tuples
+        for i in rarity_collection.items:
+            for k, v in i.traits.items():
+                if (k, v) not in rarity_collection.traits:
+                    rarity_collection.traits.append((k, v))
+                else:
+                    pass
+
+        # Loop over items in collection to add None type to items and sort and create category objects
+        for i in rarity_collection.items:
+            for t in rarity_collection.traits:
+                # If item has empty attributes/traits in categories make them explicity None
+                if t[0] not in i.traits.keys():
+                    i.traits[t[0]] = None
+                # Set up category objects in collection
+                if t[1] not in rarity_collection.categories.keys():
+                    rarity_collection.categories[t[0]] = Category(t[0])
+
+        # Loop over items in collection and count trait occurrences into category objects
+        for i in rarity_collection.items:
+            for k, v in i.traits.items():
+                #print(i.ID, t, v)
+                if t in rarity_collection.categories[k].traits:
+                    rarity_collection.categories[k].trait_count[v] += 1
+                else:
+                    rarity_collection.categories[k].traits.append(v)
+                    rarity_collection.categories[k].trait_count[v] = 1
+
+        # Loop over categories and calculate frequency and rarity score
+        for v in rarity_collection.categories.values():
+            for t in v.traits:
+                v.trait_freq[t] = v.trait_count[t]/rarity_collection.item_count
+                v.trait_rarity[t] = 1/v.trait_freq[t]
+                v.trait_rarity_normed[t] = v.trait_rarity[t]*(rarity_collection.get_avg_gm_hm()/len(v.traits))
+
+        # Loop over items and calculate statistical rarity and rarity score
+        for i in rarity_collection.items:
+            for k, v in i.traits.items():
+                i.stat_rarity = i.stat_rarity * rarity_collection.categories[k].trait_freq[v]
+                i.rarity_score = i.rarity_score + rarity_collection.categories[k].trait_rarity[v]
+                i.rarity_score_normed = i.rarity_score_normed + rarity_collection.categories[k].trait_rarity_normed[v]
+
+    for i in rarity_collection.items:
+        print(i.traits)
+        print(i.stat_rarity)
+        print(i.rarity_score)
+        print(i.rarity_score_normed)
+
+# _c = BaseCollection('non-fungible-soup')
+# c = Collection()
+# for i in range(_c.token_start, _c.token_end):
+#     with open(f'data/{_c.contract_address}/{i}.json') as f:
+#         d = f.read()
+#         print(d)
+#         data = json.loads(d)
+#         print(data)
+#         c.items.append(Item(data))
+#         c.item_count += 1
+#
+# print(c.items[0])
--- gallery/library/rarity.py
+++ gallery/library/rarity.py
@@ -4,12 +4,13 @@ from statistics import median_grouped
 from statistics import geometric_mean
 from statistics import harmonic_mean
 
-from gallery.collections import Collection as BaseCollection
-
 
 class Item:
-    '''Representation of a single item/token in a collection'''
-    def __init__(self, data):
+    """
+    Representation of a single item/token in a collection
+    """
+    def __init__(self, token_id, data):
+        self.id = token_id
         self.traits = {}
         self.traits["trait_count"] = 0
         self.stat_rarity = 1
@@ -25,11 +26,13 @@ class Item:
                 self.traits["trait_count"] += 1
 
     def __rep__(self):
-        return self.ID
+        return self.token_id
 
 
 class Category:
-    '''Contain info on category including counts and stuff'''
+    """
+    Contain info on category including counts and stuff
+    """
     def __init__(self, name):
         self.name = name
         self.traits = []
@@ -40,55 +43,63 @@ class Category:
 
 
 class Collection:
-    '''Representation of an entire collection of items'''
+    """
+    Representation of an entire collection of items
+    """
     def __init__(self):
         self.traits = []                # List of tuples coupling (category, trait)
-        self.item_count = 0             # Number of items in collection
         self.items = []                 # List of all item objects in collection
         self.trait_count = {}           # Mapping of number of traits to count
         self.categories = {}            # Dict of all categories in collection with counts and stuff
 
+    @staticmethod
+    def item_count(self):
+        return len(self.items)
+
     def get_avg_trait_per_cat(self):
-        '''Return the average number of traits per category'''
+        """
+        Return the average number of traits per category
+        """
         traits_per_cat = []
         for c in self.categories.values():
             traits_per_cat.append(len(c.traits))
         return sum(traits_per_cat)/len(traits_per_cat)
 
     def get_med_trait_per_cat(self):
+        """
+        Return the median number of traits per category
+        """
         traits_per_cat = []
         for c in self.categories.values():
             traits_per_cat.append(len(c.traits))
         return median(traits_per_cat)
 
     def get_gm_trait_per_cat(self):
+        """
+        Return the geometric mean number of traits per category
+        """
         traits_per_cat = []
         for c in self.categories.values():
             traits_per_cat.append(len(c.traits))
         return geometric_mean(traits_per_cat)
 
     def get_hm_trait_per_cat(self):
+        """
+        Return the harmonic mean number of traits per category
+        """
         traits_per_cat = []
         for c in self.categories.values():
             traits_per_cat.append(len(c.traits))
         return harmonic_mean(traits_per_cat)
 
     def get_avg_med_gm_hm(self):
+        """
+        Return the average score across median, geometric mean, and harmonic mean
+        """
         return (self.get_avg_trait_per_cat()*self.get_med_trait_per_cat()*self.get_gm_trait_per_cat()*self.get_hm_trait_per_cat())**0.25
 
     def get_avg_gm_hm(self):
+        """
+        Return the average score across geometric mean, and harmonic mean
+        """
         return (self.get_avg_trait_per_cat()*self.get_gm_trait_per_cat())**(1/2)
-
-
-_c = BaseCollection('non-fungible-soup')
-c = Collection()
-for i in range(_c.token_start, _c.token_end):
-    with open(f'data/{_c.contract_address}/{i}.json') as f:
-        d = f.read()
-        print(d)
-        data = json.loads(d)
-        print(data)
-        c.items.append(Item(data))
-        c.item_count += 1
-
-print(c.items[0])
