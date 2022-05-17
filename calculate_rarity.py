c53a8488e14b92860434de32f60fc31bb5ef307b
lza_menace <lza_menace@protonmail.com>
2022-05-17 13:12:56 -0700

    replace item with token

--- calculate_rarity.py
+++ calculate_rarity.py
@@ -34,20 +34,20 @@ for collection in [LocalCollection(k) for k in all_collections]:
                 sleep(3)
                 continue
 
-        # Loop over all token metadata and add items to collection and count them
+        # Loop over all token metadata and add tokens to collection and count them
         new_item = Item(token_id, data)
-        rarity_collection.items.append(new_item)
+        rarity_collection.tokens.append(new_item)
 
-        # Loop over items in collection to get list of all category/trait tuples
-        for i in rarity_collection.items:
+        # Loop over tokens in collection to get list of all category/trait tuples
+        for i in rarity_collection.tokens:
             for k, v in i.traits.items():
                 if (k, v) not in rarity_collection.traits:
                     rarity_collection.traits.append((k, v))
                 else:
                     pass
 
-        # Loop over items in collection to add None type to items and sort and create category objects
-        for i in rarity_collection.items:
+        # Loop over tokens in collection to add None type to tokens and sort and create category objects
+        for i in rarity_collection.tokens:
             for t in rarity_collection.traits:
                 # If item has empty attributes/traits in categories make them explicity None
                 if t[0] not in i.traits.keys():
@@ -56,8 +56,8 @@ for collection in [LocalCollection(k) for k in all_collections]:
                 if t[1] not in rarity_collection.categories.keys():
                     rarity_collection.categories[t[0]] = Category(t[0])
 
-        # Loop over items in collection and count trait occurrences into category objects
-        for i in rarity_collection.items:
+        # Loop over tokens in collection and count trait occurrences into category objects
+        for i in rarity_collection.tokens:
             for k, v in i.traits.items():
                 #print(i.ID, t, v)
                 if t in rarity_collection.categories[k].traits:
@@ -73,28 +73,15 @@ for collection in [LocalCollection(k) for k in all_collections]:
                 v.trait_rarity[t] = 1/v.trait_freq[t]
                 v.trait_rarity_normed[t] = v.trait_rarity[t]*(rarity_collection.get_avg_gm_hm()/len(v.traits))
 
-        # Loop over items and calculate statistical rarity and rarity score
-        for i in rarity_collection.items:
+        # Loop over tokens and calculate statistical rarity and rarity score
+        for i in rarity_collection.tokens:
             for k, v in i.traits.items():
                 i.stat_rarity = i.stat_rarity * rarity_collection.categories[k].trait_freq[v]
                 i.rarity_score = i.rarity_score + rarity_collection.categories[k].trait_rarity[v]
                 i.rarity_score_normed = i.rarity_score_normed + rarity_collection.categories[k].trait_rarity_normed[v]
 
-    for i in rarity_collection.items:
+    for i in rarity_collection.tokens:
         print(i.traits)
         print(i.stat_rarity)
         print(i.rarity_score)
         print(i.rarity_score_normed)
-
-# _c = BaseCollection('non-fungible-soup')
-# c = Collection()
-# for i in range(_c.token_start, _c.token_end):
-#     with open(f'data/{_c.contract_address}/{i}.json') as f:
-#         d = f.read()
-#         print(d)
-#         data = json.loads(d)
-#         print(data)
-#         c.items.append(Item(data))
-#         c.item_count += 1
-#
-# print(c.items[0])
--- gallery/library/rarity.py
+++ gallery/library/rarity.py
@@ -44,17 +44,17 @@ class Category:
 
 class Collection:
     """
-    Representation of an entire collection of items
+    Representation of an entire collection of tokens
     """
     def __init__(self):
         self.traits = []                # List of tuples coupling (category, trait)
-        self.items = []                 # List of all item objects in collection
+        self.tokens = []                 # List of all item objects in collection
         self.trait_count = {}           # Mapping of number of traits to count
         self.categories = {}            # Dict of all categories in collection with counts and stuff
 
     @staticmethod
     def item_count(self):
-        return len(self.items)
+        return len(self.tokens)
 
     def get_avg_trait_per_cat(self):
         """
