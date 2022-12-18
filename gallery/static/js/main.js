31fe032b987dd4cbd76ff41ba3fd1f34dd10715d
lza_menace <lza_menace@protonmail.com>
2022-12-18 12:36:35 -0800

    setting up skeleton css and reskinning gallery

--- gallery/static/js/main.js
+++ gallery/static/js/main.js
@@ -9,19 +9,6 @@ window.addEventListener('DOMContentLoaded', async () => {
     });
   }
 
-  // Menu clickable on small screens
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  if ($navbarBurgers.length > 0) {
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-      });
-    });
-  };
-
 });
 
 up.compiler('#fullscreen_btn', async function(element) {
